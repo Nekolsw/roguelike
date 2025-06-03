cad63569d97f6fe07b1c16a94671cb363cf1c7a8
Maxim <fasilev.maks@gmail.com>
2025-06-03 10:16:59 +0300

    transferring the engine to smart pointers

---
+++ XYZRoguelike/SwordMan.cpp
@@ -0,0 +1,54 @@
+#include "SwordMan.h"
+#include "CharacterStats.h"
+#include "AISwordMan.h"
+#include <ResourceSystem.h>
+#include <SpriteColliderComponent.h>
+namespace XYZRoguelike
+{
+	SwordMan::SwordMan()
+	{
+
+		gameObject = XYZEngine::GameWorld::Instance()->CreateGameObject("SwordMan");
+		auto playerRenderer = gameObject->AddComponent<XYZEngine::SpriteRendererComponent>();
+		
+		playerRenderer->SetTexture(*XYZEngine::ResourceSystem::Instance()->GetTextureShared("ball"));
+		playerRenderer->SetPixelSize(24, 24);
+		auto transform = gameObject->GetComponent<XYZEngine::TransformComponent>();
+		auto weapon = gameObject->AddComponent<XYZEngine::Weapon>();
+		auto attackSystem = gameObject->AddComponent<AttackComponent>();
+		auto movement = gameObject->AddComponent<AISwordMan>();
+		movement->SetSpeed(50.f);
+		movement->SetTarget(XYZEngine::GameWorld::Instance()->GetGameObjectByName("SwordMan"));
+
+		auto body = gameObject->AddComponent<XYZEngine::RigidbodyComponent>();
+		auto collider = gameObject->AddComponent<XYZEngine::SpriteColliderComponent>();
+		auto enemyStats = gameObject->AddComponent<XYZEngine::CharacterStats>();
+		
+	}
+	SwordMan::SwordMan(const XYZEngine::Vector2Df position)
+	{
+		gameObject = XYZEngine::GameWorld::Instance()->CreateGameObject("SwordMan");
+		auto playerRenderer = gameObject->AddComponent<XYZEngine::SpriteRendererComponent>();
+
+		playerRenderer->SetTexture(*XYZEngine::ResourceSystem::Instance()->GetTextureShared("ball"));
+		playerRenderer->SetPixelSize(24, 24);
+
+		auto transform = gameObject->GetComponent<XYZEngine::TransformComponent>();
+		transform->SetWorldPosition(position);
+		auto weapon = gameObject->AddComponent<XYZEngine::Weapon>();
+		auto attackSystem = gameObject->AddComponent<AttackComponent>();
+		auto movement = gameObject->AddComponent<AISwordMan>();
+		movement->SetSpeed(50.f);
+		movement->SetTarget(XYZEngine::GameWorld::Instance()->GetGameObjectByName("Player"));
+
+		auto body = gameObject->AddComponent<XYZEngine::RigidbodyComponent>();
+
+		auto collider = gameObject->AddComponent<XYZEngine::SpriteColliderComponent>();
+		auto enemyStats = gameObject->AddComponent<XYZEngine::CharacterStats>();
+	}
+
+	std::shared_ptr<XYZEngine::GameObject> SwordMan::GetGameObject()
+	{
+		return gameObject;
+	}
+}
