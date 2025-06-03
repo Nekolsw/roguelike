90bfe6e58c1f85bb37a80a9a301b90d1c289082e
Maxim <fasilev.maks@gmail.com>
2025-06-04 02:51:19 +0300

    Updating the collision tracking system
    It is now tracked using the linked method.

--- XYZRoguelike/Weapon.cpp
+++ XYZRoguelike/Weapon.cpp
@@ -3,44 +3,26 @@
 #include "CharacterStats.h"
 #include "IDelayedAction.h"
 #include <ResourceSystem.h>
-#include <SpriteColliderComponent.h>
 namespace XYZEngine
 {
 	Weapon::Weapon(GameObject* gameObject) : Component(gameObject)
 	{
 		ownerTransform = gameObject->GetComponent<TransformComponent>();
 		gameObject = GameWorld::Instance()->CreateGameObject("Sword");
-	
+
 		weaponRenderer = gameObject->AddComponent<SpriteRendererComponent>();
 		weaponRenderer->SetTexture(*ResourceSystem::Instance()->GetTextureShared("ball"));
 		weaponRenderer->SetPixelSize(0, 0);
 		auto body = gameObject->AddComponent<RigidbodyComponent>();
 		body->SetKinematic(true);
-		auto collider = gameObject->AddComponent<SpriteColliderComponent>();
+		collider = gameObject->AddComponent<SpriteColliderComponent>();
 		transform = gameObject->GetComponent<TransformComponent>();
 		transform->SetWorldPosition({ 0.f, 0.f });
-		float weaponDamage = 15.f;
-		collider->SubscribeCollision([weaponDamage, &tickDamage = std::move(linkDamageTime)](XYZEngine::Collision x)
-		{
-			CharacterStats* characterStats = x.first->GetGameObject()->GetComponent<CharacterStats>();
-			try 
-			{
-				if (!characterStats) 
-				{
-					throw std::invalid_argument("Component: CharacterStats not found\n");
-				}
-			}
-			catch (const std::invalid_argument& e)
-			{
-				LOG_WARN(e.what());
-			}
-			if (characterStats && tickDamage && *tickDamage <= 0.f)
-			{
-				characterStats->DealDamage(weaponDamage);
-				*tickDamage = 1.f;
-				std::cout << characterStats->GetHealth() << std::endl;
-			}
-		});
+		collider->SubscribeCollision(std::bind(&Weapon::DamageCollision, this, std::placeholders::_1));
+	}
+	Weapon::~Weapon()
+	{
+		collider->UnsubscribeCollision(std::bind(&Weapon::DamageCollision, this, std::placeholders::_1));
 	}
 	GameObject* Weapon::GetGameObject()
 	{
@@ -49,22 +31,25 @@ namespace XYZEngine
 
 	void Weapon::Update(float deltaTime)
 	{
-		if (*linkDamageTime > 0.f)
+		if (tickDamage > 0.f)
 		{
-			*linkDamageTime -= 1.f * deltaTime;
+			tickDamage -= 1.f * deltaTime;
 		}
-		auto characterPosition = ownerTransform->GetWorldPosition();
-		auto characterRotation = ownerTransform->GetWorldRotation();
-		// Обновляем позицию меча
-		float dirX = cos(transform->GetWorldRotation() * 3.14159265f / 180.f); //Переводим в радианы 
-		float dirY = sin(transform->GetWorldRotation() * 3.14159265f / 180.f);
+		if (ownerTransform)
+		{
+			auto characterPosition = ownerTransform->GetWorldPosition();
+			auto characterRotation = ownerTransform->GetWorldRotation();
+			// Обновляем позицию меча
+			float dirX = cos(transform->GetWorldRotation() * 3.14159265f / 180.f); //Переводим в радианы 
+			float dirY = sin(transform->GetWorldRotation() * 3.14159265f / 180.f);
 
-		Vector2Df offset;
-		offset.x = characterPosition.x + dirX * placementDistance;
-		offset.y = characterPosition.y + dirY * placementDistance;
+			Vector2Df offset;
+			offset.x = characterPosition.x + dirX * placementDistance;
+			offset.y = characterPosition.y + dirY * placementDistance;
 
-		transform->SetWorldRotation(characterRotation);
-		transform->SetWorldPosition(offset);
+			transform->SetWorldRotation(characterRotation);
+			transform->SetWorldPosition(offset);
+		}
 	}
 
 	void Weapon::Render()
@@ -77,12 +62,31 @@ namespace XYZEngine
 		{
 			weaponRenderer->SetPixelSize(35, 8);
 		}
-		else 
+		else
 		{
 			weaponRenderer->SetPixelSize(0, 0);
 		}
-		
-		
+
+
+	}
+
+	void Weapon::DamageCollision(XYZEngine::Collision collision)
+	{
+		auto characterStats = collision.first->GetGameObject()->GetComponent<CharacterStats>();
+		try {
+			if (!characterStats) {
+				throw std::invalid_argument(
+					"Component: CharacterStats not found\n");
+			}
+		}
+		catch (const std::invalid_argument& e) {
+			LOG_WARN(e.what());
+		}
+		if (characterStats && (tickDamage <= 0.f && tickDamage <= 0.f)) {
+			characterStats->DealDamage(weaponDamage);
+			tickDamage = 1.f;
+			std::cout << characterStats->GetHealth() << std::endl;
+		}
 	}
 
 
--- XYZRoguelike/Weapon.h
+++ XYZRoguelike/Weapon.h
@@ -5,21 +5,26 @@
 #include "InputComponent.h"
 #include "RenderSystem.h"
 #include "GameObject.h"
+#include <SpriteColliderComponent.h>
 namespace XYZEngine
 {
 	class Weapon : public Component
 	{
 	public:
 		Weapon(GameObject* gameObject);
+		~Weapon();
 		GameObject* GetGameObject();
 		void Update(float deltaTime) override;
 		void Render() override;
 		void SetActive(bool OnActive);
+		void DamageCollision(XYZEngine::Collision collision);
 	private:
 		XYZEngine::SpriteRendererComponent* weaponRenderer;
 		XYZEngine::TransformComponent* transform;
 		XYZEngine::TransformComponent* ownerTransform;
+		XYZEngine::SpriteColliderComponent* collider;
 		float placementDistance = 40.f;
-		const std::unique_ptr<float> linkDamageTime = std::make_unique<float>(1.f);
+		float weaponDamage = 55.f;
+		float tickDamage = 0.f;
 	};
 }
