88934d68f2cd4d266cbc9aa13bf28354bbf1ab09
Maxim <fasilev.maks@gmail.com>
2025-06-19 05:23:38 +0300

    ClangFormat

--- XYZRoguelike/Player.cpp
+++ XYZRoguelike/Player.cpp
@@ -1,57 +1,61 @@
 #include "Player.h"
-#include "CharacterStats.h"
-#include "Weapon.h"
+
 #include "AttackComponent.h"
+#include "CharacterStatsComponent.h"
 #include "ControllerInputComponent.h"
-#include <ResourceSystem.h>
+#include "Explosion.h"
+#include "Sword.h"
+
 #include <MovementComponent.h>
+#include <ResourceSystem.h>
 #include <SpriteColliderComponent.h>
-#include <SpriteMovementAnimationComponent.h>
 #include <SpriteDirectionComponent.h>
+#include <SpriteMovementAnimationComponent.h>
 
-namespace XYZRoguelike
-{
-	Player::Player(const XYZEngine::Vector2Df& position)
-	{
-		gameObject = XYZEngine::GameWorld::Instance()->CreateGameObject("Player");
+namespace XYZRoguelike {
+Player::Player(const XYZEngine::Vector2Df& position) {
+    gameObject = XYZEngine::GameWorld::Instance()->CreateGameObject("Player");
 
+    auto playerRenderer =
+        gameObject->AddComponent<XYZEngine::SpriteRendererComponent>();
+    playerRenderer->SetTexture(
+        *XYZEngine::ResourceSystem::Instance()->GetTextureMapElementShared(
+            "player", 0));
+    playerRenderer->SetPixelSize(32, 32);
 
-		auto playerRenderer = gameObject->AddComponent<XYZEngine::SpriteRendererComponent>();
-		playerRenderer->SetTexture(*XYZEngine::ResourceSystem::Instance()->GetTextureMapElementShared("player", 0));
-		playerRenderer->SetPixelSize(32, 32);
+    auto playerCamera = gameObject->AddComponent<XYZEngine::CameraComponent>();
+    playerCamera->SetWindow(
+        &XYZEngine::RenderSystem::Instance()->GetMainWindow());
+    playerCamera->SetBaseResolution(1280, 720);
+    playerCamera->SetTurnTracking(false);
 
-		auto playerCamera = gameObject->AddComponent<XYZEngine::CameraComponent>();
-		playerCamera->SetWindow(&XYZEngine::RenderSystem::Instance()->GetMainWindow());
-		playerCamera->SetBaseResolution(1280, 720);
-		playerCamera->SetTurnTracking(false);
+    auto playerInput = gameObject->AddComponent<XYZEngine::InputComponent>();
 
-		auto playerInput = gameObject->AddComponent<XYZEngine::InputComponent>();
+    auto transform = gameObject->GetComponent<XYZEngine::TransformComponent>();
+    transform->SetWorldPosition(position);
 
-		auto transform = gameObject->GetComponent<XYZEngine::TransformComponent>();
-		transform->SetWorldPosition(position);
+    auto movement = gameObject->AddComponent<XYZEngine::MovementComponent>();
+    movement->SetSpeed(400.f);
+    movement->SetDirectionalRotation(true);
 
-		auto movement = gameObject->AddComponent<XYZEngine::MovementComponent>();
-		movement->SetSpeed(400.f);
-		movement->SetDirectionalRotation(true);
+    auto spriteDirection =
+        gameObject->AddComponent<XYZEngine::SpriteDirectionComponent>();
 
+    auto body = gameObject->AddComponent<XYZEngine::RigidbodyComponent>();
+    auto weapon = gameObject->AddComponent<Sword>();
 
-		auto spriteDirection = gameObject->AddComponent<XYZEngine::SpriteDirectionComponent>();
+    auto attackSystem = gameObject->AddComponent<AttackComponent>();
+    auto controllerInput = gameObject->AddComponent<ControllerInputComponent>();
 
-		auto body = gameObject->AddComponent<XYZEngine::RigidbodyComponent>();
-		auto weapon = gameObject->AddComponent<XYZEngine::Weapon>();
+    auto collider =
+        gameObject->AddComponent<XYZEngine::SpriteColliderComponent>();
 
+    auto animator =
+        gameObject->AddComponent<XYZEngine::SpriteMovementAnimationComponent>();
+    animator->Initialize("player", 6.f);
 
-		auto attackSystem = gameObject->AddComponent<AttackComponent>();
-		auto controllerInput = gameObject->AddComponent<ControllerInputComponent>();
-
-		auto collider = gameObject->AddComponent<XYZEngine::SpriteColliderComponent>();
-
-		auto animator = gameObject->AddComponent<XYZEngine::SpriteMovementAnimationComponent>();
-		animator->Initialize("player", 6.f);
-
-		auto playerStats = gameObject->AddComponent<XYZEngine::CharacterStats>();
-		playerStats->Setarmor(4.f);
-
-	}
-
+    auto playerStats = gameObject->AddComponent<CharacterStatsComponent>();
+    playerStats->SetArmor(4.f);
 }
+
+}  // namespace XYZRoguelike
--- XYZRoguelike/Player.h
+++ XYZRoguelike/Player.h
@@ -1,19 +1,18 @@
 #pragma once
 
 #include "CameraComponent.h"
-#include "GameWorld.h"
-#include "SpriteRendererComponent.h"
-#include "RenderSystem.h"
-#include "InputComponent.h"
 #include "GameObject.h"
+#include "GameWorld.h"
+#include "InputComponent.h"
+#include "RenderSystem.h"
+#include "SpriteRendererComponent.h"
 
-namespace XYZRoguelike
-{
-	class Player
-	{
-	public:
-		Player(const XYZEngine::Vector2Df& position);
-	private:
-		XYZEngine::GameObject* gameObject;
-	};
-}
+namespace XYZRoguelike {
+class Player {
+   public:
+    Player(const XYZEngine::Vector2Df& position);
+
+   private:
+    XYZEngine::GameObject* gameObject;
+};
+}  // namespace XYZRoguelike
