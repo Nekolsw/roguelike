4b6adfe2174a2e1d059d0e714b397cd69d6b3541
Maxim <fasilev.maks@gmail.com>
2025-06-19 05:14:48 +0300

    ClangFormat

--- XYZRoguelike/AttackComponent.h
+++ XYZRoguelike/AttackComponent.h
@@ -1,20 +1,18 @@
 #pragma once
 #include "TransformComponent.h"
 #include "Weapon.h"
-namespace XYZRoguelike
-{
-	class AttackComponent : public XYZEngine::Component
-	{
-	public:
-		AttackComponent(XYZEngine::GameObject* gameObject);
-		void Attack();
+namespace XYZRoguelike {
+class AttackComponent : public XYZEngine::Component {
+   public:
+    AttackComponent(XYZEngine::GameObject* gameObject);
+    void Attack();
 
-	private:
-		void Update(float deltaTime) override;
-		void Render() override;
-		XYZEngine::Weapon* weapon;
-		float delayAttack = 0.f;
-		float attackSpeed = 0.f;
-	};
+   private:
+    void Update(float deltaTime) override;
+    void Render() override;
+    Weapon* weapon;
+    float delayAttack = 0.f;
+    float attackSpeed = 0.f;
+};
 
-}
+}  // namespace XYZRoguelike
