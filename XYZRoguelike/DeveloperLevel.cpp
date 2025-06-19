9b0722a3a0513553a1b5228afeae18eb1bf9e9f7
Maxim <fasilev.maks@gmail.com>
2025-06-19 05:30:33 +0300

    Add NPCSpawner

--- XYZRoguelike/DeveloperLevel.cpp
+++ XYZRoguelike/DeveloperLevel.cpp
@@ -1,96 +1,121 @@
 #include "DeveloperLevel.h"
-#include "MazeGenerator.h"
 
+#include "AICreeper.h"
+#include "Explosion.h"
+#include "MazeGenerator.h"
+#include "NPCSpawner.h"
+#include "Sword.h"
 
 using namespace XYZEngine;
 
-namespace XYZRoguelike
-{
-	void DeveloperLevel::Start()
-	{
-		int width = 17;
-		int height = 17;
-		int x_Offset = 0.f;
-		int y_Offset = 0.f;
+namespace XYZRoguelike {
+void DeveloperLevel::Start() {
+    int width = 17;
+    int height = 17;
+    int x_Offset = 0.f;
+    int y_Offset = 0.f;
 
-		for (int y = 0; y < height + 1; y++)
-		{
-			for (int x = 0; x < width + 1; x++)
-			{
-				//if not wall place
-				if (x != 0 && x != width && y != 0 && y != height)
-				{
-					floors.push_back(std::make_unique<Floor>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(0)));
-				}
+    for (int y = 0; y < height + 1; y++) {
+        for (int x = 0; x < width + 1; x++) {
+            // if not wall place
+            if (x != 0 && x != width && y != 0 && y != height) {
+                floors.push_back(std::make_unique<Floor>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(0)));
+            }
 
-				//Left-bottom angle screen
-				if (x == 0 && y == 0)
-				{
-					walls.push_back(std::make_unique<Wall>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(25)));
-				}
+            // Left-bottom angle screen
+            if (x == 0 && y == 0) {
+                walls.push_back(std::make_unique<Wall>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(25)));
+            }
 
-				//Right-bottom angle screen
-				if (x == width && y == 0)
-				{
-					walls.push_back(std::make_unique<Wall>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(27)));
-				}
+            // Right-bottom angle screen
+            if (x == width && y == 0) {
+                walls.push_back(std::make_unique<Wall>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(27)));
+            }
 
-				//Left-top angle screen
-				if (x == 0 && y == height)
-				{
-					walls.push_back(std::make_unique<Wall>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(1)));
-				}
+            // Left-top angle screen
+            if (x == 0 && y == height) {
+                walls.push_back(std::make_unique<Wall>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(1)));
+            }
 
-				//Right-top angle screen
-				if (x == width && y == height)
-				{
-					walls.push_back(std::make_unique<Wall>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(3)));
-				}
+            // Right-top angle screen
+            if (x == width && y == height) {
+                walls.push_back(std::make_unique<Wall>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(3)));
+            }
 
-				//Left screen
-				if (x == 0 && y != height && y != 0)
-				{
-					floors.push_back(std::make_unique<Floor>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(18)));
-					walls.push_back(std::make_unique<Wall>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(12)));
-				}
+            // Left screen
+            if (x == 0 && y != height && y != 0) {
+                floors.push_back(std::make_unique<Floor>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(18)));
+                walls.push_back(std::make_unique<Wall>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(12)));
+            }
 
-				//Right screen
-				if (x == width && y != height && y != 0)
-				{
-					floors.push_back(std::make_unique<Floor>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(19)));
-					walls.push_back(std::make_unique<Wall>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(12)));
-				}
+            // Right screen
+            if (x == width && y != height && y != 0) {
+                floors.push_back(std::make_unique<Floor>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(19)));
+                walls.push_back(std::make_unique<Wall>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(12)));
+            }
 
-				//Bottom screen
-				if (y == 0 && x != width && x != 0)
-				{
-					walls.push_back(std::make_unique<Wall>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(38)));
-				}
+            // Bottom screen
+            if (y == 0 && x != width && x != 0) {
+                walls.push_back(std::make_unique<Wall>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(38)));
+            }
 
-				//Top screen
-				if (y == height && x != width && x != 0)
-				{
-					walls.push_back(std::make_unique<Wall>(std::forward<XYZEngine::Vector2Df>({ x_Offset + x * 64.f, y_Offset + y * 64.f }), std::forward<int>(38)));
-				}
-			}
-		}
-		int generatWidth = width + x_Offset;
-		int generatHeight = height + x_Offset;
-		MazeGenerator mazeGenerator(generatWidth, generatHeight, this);
-		mazeGenerator.Generate();
+            // Top screen
+            if (y == height && x != width && x != 0) {
+                walls.push_back(std::make_unique<Wall>(
+                    std::forward<XYZEngine::Vector2Df>(
+                        {x_Offset + x * 64.f, y_Offset + y * 64.f}),
+                    std::forward<int>(38)));
+            }
+        }
+    }
+    int generatWidth = width + x_Offset;
+    int generatHeight = height + x_Offset;
+    MazeGenerator mazeGenerator(generatWidth, generatHeight, this);
+    mazeGenerator.Generate();
 
-		player = std::make_shared<Player>(std::forward<XYZEngine::Vector2Df>({ (x_Offset + width) / 2 * 64.f, (y_Offset + height) / 2 * 64.f }));
-		swordMan = std::make_shared<Enemy>(XYZEngine::Vector2Df({ 350.f, 350.f }));
+    auto enemySpawner =
+        std::make_shared<NPCSpawner<Enemy<AICreeper, Explosion>>>(
+            XYZEngine::Vector2Df{400.f, 400.f}, 1.f, 3, true, "EnemyCreeper");
+    auto player = std::make_shared<Player>(std::forward<XYZEngine::Vector2Df>(
+        {(x_Offset + width) / 2 * 64.f, (y_Offset + height) / 2 * 64.f}));
+    auto swordMan = std::make_shared<Enemy<AISwordMan, Sword>>(
+        XYZEngine::Vector2Df({350.f, 350.f}), "SwordMan");
 
-		backgroundMusic = std::make_unique<SoundPlayer>("BackgroundMusic");
-	}
-	void DeveloperLevel::Restart()
-	{
-		Stop();
-		Start();
-	}
-	void DeveloperLevel::Stop() 
-	{
-		GameWorld::Instance()->Clear();
-	}
+    backgroundMusic = std::make_unique<SoundPlayer>("BackgroundMusic");
 }
+void DeveloperLevel::Restart() {
+    Stop();
+    Start();
+}
+void DeveloperLevel::Stop() { GameWorld::Instance()->Clear(); }
+}  // namespace XYZRoguelike
--- XYZRoguelike/DeveloperLevel.h
+++ XYZRoguelike/DeveloperLevel.h
@@ -1,29 +1,29 @@
 #pragma once
 
-#include <iostream>
-#include <array>
-#include "Scene.h"
-#include "Player.h"
-#include "SoundPlayer.h"
+#include "AISwordMan.h"
 #include "Enemy.h"
 #include "Floor.h"
+#include "NPCSpawner.h"
+#include "Player.h"
+#include "Scene.h"
+#include "SoundPlayer.h"
 #include "Wall.h"
+
+#include <array>
+#include <iostream>
 
 using namespace XYZEngine;
 
-namespace XYZRoguelike
-{
-	class DeveloperLevel : public Scene
-	{
-	public:
-		void Start() override;
-		void Restart() override;
-		void Stop() override;
-		std::vector<std::unique_ptr<Wall>> walls;
-		std::vector<std::unique_ptr<Floor>> floors;
-	private:
-		std::shared_ptr<Player> player;
-		std::shared_ptr<Enemy> swordMan;
-		std::unique_ptr<SoundPlayer> backgroundMusic;
-	};
-}
+namespace XYZRoguelike {
+class DeveloperLevel : public Scene {
+   public:
+    void Start() override;
+    void Restart() override;
+    void Stop() override;
+    std::vector<std::unique_ptr<Wall>> walls;
+    std::vector<std::unique_ptr<Floor>> floors;
+
+   private:
+    std::unique_ptr<SoundPlayer> backgroundMusic;
+};
+}  // namespace XYZRoguelike
